8a3ee4df19f9cad8f1e4e2228720b76c7b18f004
Hwan seok Kim <kcsss4499@gmail.com>
2025-03-18 23:54:26 +0900

    melon_data

--- selenium_melon.py
+++ selenium_melon.py
@@ -34,11 +34,53 @@ if driver.current_url != url:
 driver.find_element(By.LINK_TEXT,"멜론차트").click()
 
 driver.find_elements(By.ID,"moreBtn")[1].click()
+time.sleep(2)
 
 chat_list=driver.find_element(By.ID,"_chartList")
 
 items=chat_list.find_elements(By.CLASS_NAME,"list_item")
-print(len(items))
-driver.quit()
+
+
+
+
+
+action=ActionChains(driver)
+
+
+# element로 이동 내가원한건 90번째 위치로 가게끔
+# action.move_to_element(items[90]).perform()
+
+# 10개씩 내려서 보여줌
+for rank,item in enumerate(items[:10],1) :
+    action.move_to_element(item).perform()
+    name=item.find_element(By.CLASS_NAME,"name.ellipsis")
+    
+    title=item.find_element(By.CLASS_NAME,"title.ellipsis")
+    thumb=item.find_element(By.CLASS_NAME,"inner > span")
+    thumb.click()
+    time.sleep(1)
+    album_url=driver.current_url
+    driver.back()
+    time.sleep(1)
+
+
+    print(f"<<<{rank}>>>")
+    print(name.text)
+    print(title.text)
+    print(f"album_url : {album_url}")
+    print()
+
+    time.sleep(1)
+
+    
+
+
+
+
+# print(len(items))
+
+
+
+# driver.quit()
 
 
